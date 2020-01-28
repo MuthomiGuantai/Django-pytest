543882834962db621b947f7bb5ca84e5fe514369
Bruce Muthomi Guantai <muthomiguantai@gmail.com>
2020-01-28 21:21:37 +0300

    pytest-django setup

---
+++ products/tests/test_urls.py
@@ -0,0 +1,8 @@
+from django.urls import reverse, resolve
+
+
+class TestUrle:
+
+    def test_detail_url(self):
+        path = reverse('detail', kwargs={'pk': 1})
+        assert resolve(path).view_name == 'detail'
